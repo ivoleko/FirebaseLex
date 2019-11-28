caee16f8df9771560f93f2f0077695fe595c46ed
Ivo Leko <ivoleko@gmail.com>
2019-11-28 22:08:18 +0100

    Initial commit

---
+++ FirebaseEvolutio/FirebaseEvolutio.h
@@ -0,0 +1,22 @@
+//
+//  FirebaseEvolutio.h
+//  FirebaseEvolutio
+//
+//  Created by Ivo Leko on 28/11/2019.
+//  Copyright © 2019 Evolutio IT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseEvolutio.
+FOUNDATION_EXPORT double FirebaseEvolutioVersionNumber;
+
+//! Project version string for FirebaseEvolutio.
+FOUNDATION_EXPORT const unsigned char FirebaseEvolutioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseEvolutio/PublicHeader.h>
+
+#import <FirebaseEvolutio/Firebase.h>
+
+
+
---
+++ FirebaseEvolutio/FirebaseEvolutio.swift
@@ -0,0 +1,12 @@
+//
+//  FirebaseEvolutio.swift
+//  FirebaseEvolutio
+//
+//  Created by Ivo Leko on 28/11/2019.
+//  Copyright © 2019 Evolutio IT. All rights reserved.
+//
+
+import Foundation
+import FirebaseCore
+
+

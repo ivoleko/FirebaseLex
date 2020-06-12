2d243d4f7577ab7f4d68665179c5f7edde8500f2
ivoleko <ivoleko@gmail.com>
2020-06-12 13:37:23 +0200

    New Firebase

---
+++ FirebaseLex/FirebaseLex.h
@@ -0,0 +1,21 @@
+//
+//  FirebaseLex.h
+//  FirebaseLex
+//
+//  Created by Ivo Leko on 28/11/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseLex.
+FOUNDATION_EXPORT double FirebaseLexVersionNumber;
+
+//! Project version string for FirebaseLex.
+FOUNDATION_EXPORT const unsigned char FirebaseLexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseLex/PublicHeader.h>
+
+#import "FirebaseLex/Firebase.h"
+
+
+
---
+++ FirebaseLex/FirebaseLex.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseLex.swift
+//  FirebaseLex
+//
+//  Created by Ivo Leko on 28/11/2019.
+//
+
+import Foundation
+import Firebase
+import FirebaseAnalytics
+import FirebaseMessaging
+import FirebaseCore
+import FirebaseInstanceID
+import FirebaseInAppMessaging
